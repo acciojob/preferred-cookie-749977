eb96a3fbc8d1ab490996e4cf23d4c7ce9b44e832
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-10 14:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+        const [key, value] = cookies[i].split("=");
+        if (key === name) return value;
+    }
+    return null;
+}
+
+function applyPreferences() {
+    const savedFontSize = getCookie("fontsize");
+    const savedFontColor = getCookie("fontcolor");
+    if (savedFontSize) document.documentElement.style.setProperty("--fontsize", savedFontSize + "px");
+    if (savedFontColor) document.documentElement.style.setProperty("--fontcolor", savedFontColor);
+}
+
+document.getElementById("fontForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const fontSize = document.getElementById("fontsize").value;
+    const fontColor = document.getElementById("fontcolor").value;
+    setCookie("fontsize", fontSize, 30);
+    setCookie("fontcolor", fontColor, 30);
+    applyPreferences();
+});
+
+applyPreferences();
